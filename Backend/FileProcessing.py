f59ffd0e798b1744973ccd0ec834cb2f882edd74
kkmm1234 <keithmoffitt2001@gmail.com>
2024-12-17 17:49:00 +0000

    SCRUM-23 Restructured files and Intialised ChromaDB

---
+++ Backend/FileProcessing.py
@@ -0,0 +1,34 @@
+import chromadb
+from chromadb.config import Settings
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.schema import Document
+from pypdf import PdfReader
+
+def InitialiseChromadb():
+    #client to connect to chromadb
+    client = chromadb.Client(Settings(persist_directory ="./chromadb_store"))
+    #get or create collection
+    collection = client.get_or_create_collection(name="PdfChunks")
+    return collection
+
+#function to read PDF file
+def ExtractAndChunk(uploaded_file):
+    #extract pdf text
+    pdf_reader = PdfReader(uploaded_file)
+    full_text = ""
+    #extract text from each page
+    for page in pdf_reader.pages:
+        full_text += page.extract_text()
+    
+    #chunk text using langchain TextSplitter
+    text_splitter = CharacterTextSplitter(
+        separator="\n", #split by newline
+        chunk_size=1000, #maximun chunk size
+        chunk_overlap=100 #overlap between chunks preserves context
+    )
+    #split text into chunks
+    chunks = text_splitter.split_text(full_text)
+    #return chunks as list of Documents
+    return [Document(page_content=chunk) for chunk in chunks]
+
+
--- Pages/1Upload.py
+++ Pages/1Upload.py
@@ -1,27 +1,5 @@
 import streamlit as st
-from langchain.text_splitter import CharacterTextSplitter
-from langchain.schema import Document
-from pypdf import PdfReader
-
-#function to read PDF file
-def ExtractAndChunk(uploaded_file):
-    #extract pdf text
-    pdf_reader = PdfReader(uploaded_file)
-    full_text = ""
-    #extract text from each page
-    for page in pdf_reader.pages:
-        full_text += page.extract_text()
-    
-    #chunk text using langchain TextSplitter
-    text_splitter = CharacterTextSplitter(
-        separator="\n", #split by newline
-        chunk_size=1000, #maximun chunk size
-        chunk_overlap=100 #overlap between chunks preserves context
-    )
-    #split text into chunks
-    chunks = text_splitter.split_text(full_text)
-    #return chunks as list of Documents
-    return [Document(page_content=chunk) for chunk in chunks]
+from Backend.FileProcessing import * 
 
 #header for Upload Section
 st.header("Upload Your Documents")
